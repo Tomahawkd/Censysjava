143fa9a3f250f91da4009ad4a81a41a94a2ee9cb
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-04-21 21:39:37 +0800

    make registration automatically before parsing

--- src/main/java/io/tomahawkd/censys/module/AbstractMessage.java
+++ src/main/java/io/tomahawkd/censys/module/AbstractMessage.java
@@ -1,8 +1,51 @@
 package io.tomahawkd.censys.module;
 
-public class AbstractMessage implements Message {
+import com.google.gson.GsonBuilder;
+import io.tomahawkd.censys.module.util.TypeAdapterRegister;
+import org.jetbrains.annotations.Contract;
+
+import java.lang.reflect.Field;
+
+public abstract class AbstractMessage implements Message {
+
+	private GsonBuilder builder = new GsonBuilder();
+
+	public Message parse(String data) {
+		register(builder);
+		return builder.create().fromJson(data, this.getClass());
+	}
+
+	@Contract("_ -> param1")
+	public GsonBuilder register(GsonBuilder builder) {
+		Field[] fields = this.getClass().getFields();
+		for (Field field : fields) {
+			Class type = field.getType();
+			if (Message.class.isAssignableFrom(type)) {
+				try {
+					((Message) field.getType().newInstance()).register(builder);
+				} catch (InstantiationException | IllegalAccessException e) {
+					e.printStackTrace();
+				}
+			} else if (TypeAdapterRegister.getInstance().isRegistered(type)) {
+				TypeAdapterRegister.getInstance().registerToBuilder(builder, type);
+			}
+		}
+
+		return builder;
+	}
+
+	@Override
+	public String buildJson() {
+		return toString();
+	}
+
 	@Override
 	public String getMessage() {
 		return toString();
+	}
+
+	@Override
+	public String toString() {
+		return register(new GsonBuilder()).create().toJson(this);
 	}
 }
--- src/main/java/io/tomahawkd/censys/module/Message.java
+++ src/main/java/io/tomahawkd/censys/module/Message.java
@@ -1,6 +1,16 @@
 package io.tomahawkd.censys.module;
 
+import com.google.gson.GsonBuilder;
+import org.jetbrains.annotations.Contract;
+
 public interface Message {
 
+	Message parse(String data);
+
+	@Contract("_ -> param1")
+	GsonBuilder register(GsonBuilder builder);
+
 	String getMessage();
+
+	String buildJson();
 }
