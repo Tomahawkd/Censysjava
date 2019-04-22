d89d8d3196858ac9223235af3cdea69fbedf2e4b
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-04-22 21:48:17 +0800

    Using JsonAdapter annotation instead

--- src/main/java/io/tomahawkd/censys/module/AbstractMessage.java
+++ src/main/java/io/tomahawkd/censys/module/AbstractMessage.java
@@ -1,47 +1,16 @@
 package io.tomahawkd.censys.module;
 
 import com.google.gson.GsonBuilder;
-import io.tomahawkd.censys.module.util.TypeAdapterRegister;
-import org.jetbrains.annotations.Contract;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
 
 public abstract class AbstractMessage implements Message {
 
-	private transient GsonBuilder builder;
-
-	protected AbstractMessage() {
-		builder = register(new GsonBuilder());
-	}
-
 	public Message parse(String data) {
-		return builder.create().fromJson(data, this.getClass());
-	}
-
-	@Contract("_ -> param1")
-	private GsonBuilder register(GsonBuilder builder) {
-		Field[] fields = this.getClass().getFields();
-		for (Field field : fields) {
-			if (Modifier.isTransient(field.getModifiers())) continue;
-			Class type = field.getType();
-			if (AbstractMessage.class.isAssignableFrom(type)) {
-				try {
-					((AbstractMessage) field.getType().newInstance()).register(builder);
-				} catch (InstantiationException | IllegalAccessException e) {
-					e.printStackTrace();
-				}
-			} else if (TypeAdapterRegister.getInstance().isRegistered(type)) {
-				TypeAdapterRegister.getInstance().registerToBuilder(builder, type);
-			}
-		}
-
-		return builder;
+		return new GsonBuilder().create().fromJson(data, this.getClass());
 	}
 
 	@Override
 	public String buildJson() {
-		return builder.create().toJson(this);
+		return new GsonBuilder().create().toJson(this);
 	}
 
 	@Override
