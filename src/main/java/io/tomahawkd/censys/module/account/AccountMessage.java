6e632e1223d378bb81c25f77237a0ad016994a6a
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-04-22 21:48:32 +0800

    Using JsonAdapter annotation instead

--- src/main/java/io/tomahawkd/censys/module/account/AccountMessage.java
+++ src/main/java/io/tomahawkd/censys/module/account/AccountMessage.java
@@ -1,7 +1,9 @@
 package io.tomahawkd.censys.module.account;
 
+import com.google.gson.annotations.JsonAdapter;
 import com.google.gson.annotations.SerializedName;
 import io.tomahawkd.censys.module.AbstractMessage;
+import io.tomahawkd.censys.module.util.DateConverter;
 
 import java.util.Date;
 
@@ -10,8 +12,10 @@ public class AccountMessage extends AbstractMessage {
 	@SerializedName("login")
 	private String loginName;
 	private String email;
+	@JsonAdapter(DateConverter.class)
 	@SerializedName("first_login")
 	private Date firstLogin;
+	@JsonAdapter(DateConverter.class)
 	@SerializedName("last_login")
 	private Date lastLogin;
 	private Quota quota;
@@ -19,6 +23,7 @@ public class AccountMessage extends AbstractMessage {
 	private class Quota extends AbstractMessage {
 		private int used;
 		@SerializedName("resets_at")
+		@JsonAdapter(DateConverter.class)
 		private Date resetTime;
 		private int allowance;
 	}
